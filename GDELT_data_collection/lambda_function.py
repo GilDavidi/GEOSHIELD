b1286aeed8d81eb2c531cd94fb8d5ea025dbc51c
gildavidi75@gmail.com <ec2-user@ip-172-31-45-57.eu-west-1.compute.internal>
2024-04-25 07:58:00 +0000

    Update Lambda function code

---
+++ GDELT_data_collection/lambda_function.py
@@ -0,0 +1,88 @@
+import json
+import boto3
+from datetime import datetime
+import requests
+import configparser
+from newspaper import Article
+
+# AWS S3 client
+s3 = boto3.client('s3')
+lambda_client = boto3.client('lambda')
+
+# Define the base URL for the GDELT API
+gdelt_api_url = "https://api.gdeltproject.org/api/v2/doc/doc"
+
+def lambda_handler(event, context):
+    try:
+        # Read category from the event
+        category = event['category']
+
+        # Read configuration
+        config = configparser.ConfigParser()
+        config.read("config.ini")
+
+        # Fetch GDELT articles based on category
+        gdelt_data = make_gdelt_request(config, category)
+        article_list = extract_articles(gdelt_data['articles'])
+
+        # Convert article list to JSON
+        json_data = json.dumps(article_list)
+        
+        # Invoke destination Lambda function
+        invoke_destination_lambda(json_data)
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": f"{category.capitalize()} GDELT articles fetched and sent to destination Lambda"})
+        }
+    except KeyError as e:
+        return {
+            "errorMessage": str(e),
+            "errorType": type(e)._name_
+        }
+
+def make_gdelt_request(config, category):
+    keyword_bank = config['GDELT'][category]  # Access the keyword bank based on the category
+    keyword_query = ' OR '.join(keyword_bank.split(','))
+
+    params = {
+        'format': 'JSON',
+        'timespan': '48H',
+        'query': f'({keyword_query}) sourcelang:eng',
+        'mode': 'artlist',
+        'maxrecords': 80,
+        'sort': 'hybridrel'
+    }
+
+    url = gdelt_api_url + '?' + '&'.join([f'{key}={value}' for key, value in params.items()])
+    print(url)
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+def extract_articles(articles):
+    extracted_articles = []
+
+    for article in articles:
+        article_title = article['title']
+        article_url = article['url']
+        article_date = datetime.strptime(article['seendate'], "%Y%m%dT%H%M%SZ").strftime("%Y-%m-%dT%H:%M:%S+00:00")
+        extracted_article = {
+                "title": article_title,
+                "date": article_date,
+                "url": article_url,}
+        extracted_articles.append(extracted_article)
+
+    return extracted_articles
+
+def invoke_destination_lambda(json_data):
+    # Invoke the destination Lambda function
+    lambda_client.invoke(
+        FunctionName='summarize_articles_gdelt',
+        InvocationType='Event',  # Asynchronous invocation
+        Payload=json.dumps({'json_data': json_data})
+    )
---
+++ data_corellation/lambda_function.py
@@ -0,0 +1,88 @@
+import json
+import boto3
+from simphile import jaccard_similarity
+import Levenshtein
+from datetime import datetime, timedelta
+
+def load_json_from_s3(bucket_name, file_key):
+    s3 = boto3.client('s3')
+    response = s3.get_object(Bucket=bucket_name, Key=file_key)
+    return json.loads(response['Body'].read().decode('utf-8'))
+
+def levenshtein_similarity(text1, text2):
+    return 1 - Levenshtein.distance(text1, text2) / max(len(text1), len(text2))
+
+def find_matching_messages(telegram_messages, gdelt_messages):
+    matching_messages = []
+
+    for telegram_message in telegram_messages:
+        for gdelt_message in gdelt_messages:
+            if telegram_message.get("classification") == "security" and gdelt_message.get("classification") == "security":
+                telegram_timestamp = datetime.fromisoformat(telegram_message.get("date"))
+                gdelt_timestamp = datetime.fromisoformat(gdelt_message.get("date"))
+                time_difference = abs(telegram_timestamp - gdelt_timestamp)
+
+                # Check if the time difference is within 6 hours
+                if time_difference <= timedelta(hours=6):
+                    telegram_text = telegram_message.get("message")
+                    telegram_location = telegram_message.get("location")
+                    gdelt_text = gdelt_message.get("message")
+                    levenshtein_sim = levenshtein_similarity(telegram_text , gdelt_text)
+                    jaccard_sim = jaccard_similarity(telegram_text , gdelt_text)
+                    
+                    # Decide if there is a match based on some threshold
+                    if jaccard_sim > 0.1 and levenshtein_sim > 0.1:
+                        matching_messages.append({
+                            "Telegram_id": telegram_message.get('id'),
+                            "GDELT_id": gdelt_message.get('id'),
+                            "Telegram_message": telegram_text,
+                            "GDELT_message": gdelt_text,
+                            "jaccard_similarity": jaccard_sim,
+                            "levenshtein_similarity": levenshtein_sim,
+                            "location": telegram_location
+                        })
+
+    return matching_messages
+
+def remove_duplicates(matching_messages):
+    # Create a dictionary to store the best match for each message
+    best_matches = {}
+
+    for message in matching_messages:
+        telegram_id = message["Telegram_id"]
+        if telegram_id not in best_matches or message["jaccard_similarity"] > best_matches[telegram_id].get("jaccard_similarity", 0):
+            best_matches[telegram_id] = message
+
+    return list(best_matches.values())
+
+def upload_to_s3(bucket_name, file_key, file_path):
+    s3 = boto3.client('s3')
+    s3.upload_file(file_path, bucket_name, file_key)
+
+def lambda_handler(event, context):
+    # S3 bucket name and file keys
+    input_bucket_name = "classified-data-geoshield"
+    output_bucket_name = "maching-events-geoshield"
+    telegram_file_key = "telegram_messages_classified.json"
+    gdelt_file_key = "gdelt_articles_classified.json"
+    
+    # Load JSON files from S3
+    telegram_messages = load_json_from_s3(input_bucket_name, telegram_file_key)
+    gdelt_messages = load_json_from_s3(input_bucket_name, gdelt_file_key)
+
+    matching_messages = find_matching_messages(telegram_messages, gdelt_messages)
+    unique_matching_messages = remove_duplicates(matching_messages)
+
+    # Export matching messages to a temporary file
+    output_file_path = "/tmp/matching_messages.json"
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        json.dump(unique_matching_messages, file, indent=4)
+
+    # Upload the output JSON file to the output bucket
+    upload_to_s3(output_bucket_name, "matching_messages.json", output_file_path)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Matching messages processed successfully and stored in maching-events-geoshield bucket')
+    }
+
